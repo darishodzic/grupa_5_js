d5108459893b805be8af870953695c94e380285b
Daris <daris.hodzic.dh@gmail.com>
2021-11-07 19:04:44 +0100

    commit

---
+++ app.js
@@ -0,0 +1,299 @@
+// var1 = "Hello world";
+// H e l l o   w o r l d
+// 0 1 2 3 4 5 6 7 8 9 10
+
+// var2 = var1.slice(0, 5);
+
+// console.log(var2);
+
+// text = "Please  visit Microsoft!";
+
+// newText = text.replace("Microsoft", "Javascript");
+// console.log(text);
+// console.log(newText);
+
+// text = "Dobar DAn sVima";
+
+// var1 = text.toLowerCase();
+
+// var2 = text.toUpperCase();
+// console.log(var1);
+
+// console.log(var2);
+
+// text = "Hello";
+// console.log(text);
+
+// var1 = text.concat(" ", "world");
+// console.log(var1);
+
+// text = "Hello world";
+
+// text2 = text + " " + "Daris";
+
+// console.log(text2);
+
+// var1 = 52;
+// console.log(typeof var1.toString());
+
+// var1 = "" && true;
+
+// console.log("Daris");
+
+// var1 = ["apple", "banana", "kiwi"];
+
+// var2 = var1;
+
+// var1[0] = "nestp";
+
+// console.log("Ovo je prvi niz: ", var1);
+// console.log("Ovo je drugi niz: ", var2);
+
+// a = 5;
+//
+// b = a;
+//
+// a = 7;
+//
+// console.log(a);
+// console.log(b);
+
+// var1 = ["Daris", 297, "Umejr", true];
+
+// var1[2] = "Omer";
+
+// console.log(var1);
+
+// var1 = ["Banana", "Orange", "Apple"];
+
+// console.log(var1);
+
+// var1.push(45);
+
+// console.log(var1);
+
+// var1 = "Hello";
+
+// var1 = var1.concat(" ", "World", "4556");
+
+// console.log(var1);
+
+// console.log(5 !== "5");
+
+// var1 = ["Banana", "Orange", "Apple"];
+
+// var2 = var1.join("JS");
+
+// console.log(var1);
+
+// console.log(var2);
+
+// var1 = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(var1);
+// vracenaVrednost = var1.pop();
+
+// console.log(var1);
+
+// console.log(vracenaVrednost);
+
+// var1 = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(var1);
+//
+// vracenaVrednost = var1.pop();
+// console.log(var1);
+//
+// console.log(vracenaVrednost);
+//
+
+// var1 = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(var1);
+
+// vracenaVrednost = var1.unshift("Kiwi");
+// console.log(var1);
+
+// console.log(vracenaVrednost);
+
+// var1 = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(var1);
+
+// delete var1[1];
+
+// console.log(var1);
+
+// var1 = ["Banana", "Orange", "Apple", "Mango"];
+
+// console.log("Stari array: ", var1);
+
+// var1.splice(1, 2, "prva");
+
+// console.log("Novi array: ", var1);
+
+// var1 = ["Banana", "Orange", "Apple", "Mango"];
+
+// var2 = var1.slice(0, 2);
+
+// console.log(var2);
+
+// unetiBroj = parseInt(prompt("Unesi broj"));
+
+// console.log(unetiBroj);
+
+// console.log(typeof unetiBroj);
+
+// a = parseInt(unetiBroj);
+// console.log(a);
+
+// console.log(typeof a);
+
+// console.log(`lasdjlsd`);
+
+// lista = ["hleb", "mleko"];
+
+// odabir = parseInt(prompt("unesi 1 ,unesi 2"));
+
+// if (odabir === 1) {
+//   el = prompt("Unesiite namirnicu");
+
+//   if (el.length < 8) {
+//     lista.push(el);
+//   } else {
+//     console.log("Dugacka rec");
+//   }
+// }
+// if (odabir === 2) {
+//   lista.pop();
+//   console.log(lista);
+// }
+
+// unos = parseInt(prompt("unesi broj"));
+
+// if (unos % 2 === 0) {
+//   alert("Broj paran");
+// }
+// if (unos % 2 === 1) {
+//   alert("Broj je neparan");
+// }
+
+// a = "Headasdasdasdasdasd";
+
+// if (a.length > 3 && a.length < 15) {
+//   console.log("nestoooo");
+// }
+
+// unos = parseInt(prompt("Unesite broj za proveru"));
+
+// if (unos % 3 === 0 && unos % 5 === 0) {
+//   console.log("fizzbuzz");
+// } else if (unos % 3 === 0) {
+//   console.log("fizz");
+// }. else if (unos % 5 === 0) {
+//   console.log("buzz");
+// } else {
+//   console.log("broj nije deljiv ni sa 3 ni sa 5");
+// }
+
+// var1 = parseInt(prompt("Unesi prvi broj"));
+// var2 = parseInt(prompt("Unesi drugi broj"));
+
+// if (var1 > var2) {
+//   console.log("vece je", var1);
+// } else if (var2 > var1) {
+//   console.log("vece je", var2);
+// } else {
+//   console.log("Jednaki su");
+// }
+
+// unos = prompt("Unesi ime");
+
+// switch (unos) {
+//   case "Daris":
+//     console.log("Daris jeee");
+//     break;
+//   case "Umejr":
+//     console.log("Umejr jeee");
+//     break;
+//   default:
+//     console.log("Pogresan unos");
+// }
+
+// unos = prompt("Unesi dan u nedelji");
+
+// switch (unos.toLowerCase()) {
+//   case "nedelja":
+//     console.log("Danas je nedelja");
+//     break;
+//   case "subota":
+//     console.log("Danas je subota");
+//     break;
+//   case "petak":
+//     console.log("Danas je petak");
+//     break;
+//   case "cetvrtak":
+//     console.log("Danas je cetvrtak");
+//     break;
+//   case "sreda":
+//     console.log("Danas je sreda");
+//     break;
+//   case "utorak":
+//     console.log("Danas je utorak");
+//     break;
+//   case "ponedeljak":
+//     console.log("Danas je ponedeljak");
+//     break;
+// }
+
+// a = Math.random();
+
+// console.log(a);
+
+// var1 = ["banana", "kivi", "apple"];
+
+// for (i = 0; i < var1.length; i++) {
+//   console.log(var1[i]);
+// }
+
+// for (i = 6; i < 5; i++) {
+//   console.log("asdasd");
+// }
+
+// brojPonavljanja = parseInt(prompt("Unesite broj ponavljanja"));
+// unos = prompt("unesite nesto");
+
+// for (i = 0; i < brojPonavljanja; i++) {
+//   console.log("vas unos je ", unos);
+// }
+
+// pocetnaVrednost = parseInt(prompt("Unesi pocetnu vrednost"));
+// krajnjaVrednost = parseInt(prompt("Unesi krajnju vrednost"));
+
+// for (i = pocetnaVrednost; i <= krajnjaVrednost; i++) {
+//   //   if (i % 2 === 0) {
+
+//   //     console.log("broj je paran", i);
+//   //   } else {
+//   //     console.log("broj je neparan", i);
+//   //   }
+// }
+
+// 3
+// 7
+
+// [3,4,5,6,7]
+
+// zbir = 25
+
+// pocetnaV = parseInt(prompt("Unesi pocetak"));
+// krajnjaV = parseInt(prompt("Unesi kraj"));
+
+// brojevi = [];
+// zbir = 0;
+
+// for (i = pocetnaV; i <= krajnjaV; i++) {
+//   brojevi.push(i);
+//   zbir += i;
+// }
+
+// console.log(brojevi);
+// console.log(zbir);
+
+a = ["Banana", "Jabuka", "Ananas", "Grozdje"];
