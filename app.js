f6f9c190d85d5e0f44eec263928f88cdd688c468
darishodzic <daris.hodzic.dh@gmail.com>
2021-11-14 20:45:53 +0100

    commit

--- app.js
+++ app.js
@@ -431,3 +431,41 @@
 // }
 
 // -----------------------------------------------------------------------------------------------------------------
+
+happy_nums = [];
+
+new_num = 0;
+
+start = 4;
+
+pocetniBroj = 1;
+
+// limit = 5
+limit = parseInt(prompt("Unesite koliko happy numbera zelite da dobijete:"));
+
+while (true) {
+  if (happy_nums.length === limit) {
+    break;
+  }
+  num = pocetniBroj;
+  while (true) {
+    for (j = 0; j < String(num).length; j++) {
+      new_num += parseInt(String(num)[j]) ** 2;
+    }
+    if (new_num === 1) {
+      happy_nums.push(pocetniBroj);
+      new_num = 0;
+      break;
+    } else if (new_num === start) {
+      new_num = 0;
+      break;
+    }
+    num = new_num;
+    new_num = 0;
+  }
+  pocetniBroj++;
+}
+
+console.log(happy_nums);
+
+// -----------------------------------------------------------------------------------------------------------------
